dae6d95590a0b36f640958f93d24c5494353533d
Amber  Lowe <52145666+amberlowe1001@users.noreply.github.com>
2019-12-20 12:21:57 -0800

    mvp reached one strech complete

--- index.js
+++ index.js
@@ -191,8 +191,10 @@ function sortCarInventory(/* code here */) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+    const modelYears = {
+        
+    }
 }
 
 /**
